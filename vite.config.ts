7aa593466debaa0bd5006e79f578e6aa4e094ec9
Victor Veiga <44455077+victorveiga@users.noreply.github.com>
2025-08-11 17:07:12 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -19,7 +22,8 @@ export default defineConfig({
     outDir: "dist",
     sourcemap: true,
   },
-  define: {
-    "process.env": {},
-  },
+    define: {
+      "process.env": {},
+    },
+  };
 });
