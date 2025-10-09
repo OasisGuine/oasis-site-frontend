f5cafb9737a4d259172b917aeb1b15ae7757eaa6
OasisGuine <bleao.abreu@outlook.pt>
2025-10-09 21:27:47 +0100

    upt_timer HeroSection.tsx

--- src/components/layouts/HeroSection.tsx
+++ src/components/layouts/HeroSection.tsx
@@ -22,7 +22,7 @@ const HeroSection: React.FC<Props> = (props) => {
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
-        }, 15000);
+        }, 12000);
         return () => clearInterval(interval);
     }, [currentIndex]);
 
